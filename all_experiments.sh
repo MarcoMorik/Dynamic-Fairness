aa8808e593c9de72281c56667a90498722c6a380
Marco <m.morik@tu-berlin.de>
2020-05-14 13:39:24 +0200

    all experiments

---
+++ all_experiments.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+python main.py with 'EXPERIMENT=1' 'PLOT_PREFIX="plots/EXP1/"'
+python main.py with 'EXPERIMENT=2' 'PLOT_PREFIX="plots/EXP2/"'
+python main.py with 'EXPERIMENT=3' 'PLOT_PREFIX="plots/EXP3/"'
+python main.py with 'EXPERIMENT=4' 'PLOT_PREFIX="plots/EXP4/"'
+python main.py with 'EXPERIMENT=5' 'PLOT_PREFIX="plots/EXP5/"'
+python main.py with 'EXPERIMENT=6' 'PLOT_PREFIX="plots/EXP6/"'
+python main.py with 'EXPERIMENT=7' 'PLOT_PREFIX="plots/EXP7/"'
+python main.py with 'EXPERIMENT=8' 'PLOT_PREFIX="plots/EXP8/"'
+python main.py with 'EXPERIMENT=9' 'PLOT_PREFIX="plots/EXP9/"'
+python main.py with 'EXPERIMENT=10' 'PLOT_PREFIX="plots/EXP10/"'
+
